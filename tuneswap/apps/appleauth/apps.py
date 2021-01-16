b8f121243cb36b773b011ce87ed164b09fd36bde
sjkchang <sjkchang@gmail.com>
2021-01-15 18:56:27 -0800

    Removed auth apps to declutter project prior to actually needing the apps functionality

--- tuneswap/apps/appleauth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppleauthConfig(AppConfig):
-    name = 'appleauth'
--- tuneswap/apps/youtubeauth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class YoutubeauthConfig(AppConfig):
-    name = 'youtubeauth'
